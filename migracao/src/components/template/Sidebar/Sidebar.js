69c2a5d00ca75191ed049990e21fd3e062a9dde5
H-desktop <heekbs@gmail.com>
2021-11-03 16:07:58 -0300

    tentando arrumar sidebar

--- migracao/src/components/template/Sidebar/Sidebar.js
+++ migracao/src/components/template/Sidebar/Sidebar.js
@@ -3,6 +3,18 @@ import { ListGroup } from 'react-bootstrap';
 import '../../../assets/css/style.css';
 import Logo from '../../../assets/images/logo.png';
 
+function toggleMenu() {
+    let body = document.querySelector("body");
+    let hasCollapsed = body.classList.contains("sidebar-menu-collapsed");
+    if(hasCollapsed) {
+     body.classList.remove("sidebar-menu-collapsed");
+     body.classList.add("noscroll");
+    } else {
+      body.classList.remove("noscroll");
+      body.classList.add("sidebar-menu-collapsed");
+    };
+  }
+
 function Sidebar(props) {
 
     return (
@@ -13,28 +25,28 @@ function Sidebar(props) {
 
                     {/* <!-- logo start --> */}
                     <div class="logo">
-                        <h1><a href="">Collective</a></h1>
+                        <h1><a href="index">Collective</a></h1>
                     </div>
 
                     {/* <!-- //image logo --> */}
                     <div class="logo-icon text-center">
-                        <a href="index.html" title="logo"><img src={Logo} alt="logo-icon" /> </a>
+                        <a href="index" title="logo"><img src={Logo} alt="logo-icon" /> </a>
                     </div>
                     {/* <!-- //logo end --> */}
 
                     <div class="sidebar-menu-inner">
 
                         {/* <!-- sidebar nav start --> */}
-                        <ListGroup class="nav nav-pills nav-stacked custom-nav">
-                            <ListGroup.Item><a href="index"><i class="fa fa-tachometer"></i><span> Dashboard</span></a></ListGroup.Item>
-                            <ListGroup.Item><a href="cards"><i class="fa fa-cogs"></i> <span>Default cards</span></a></ListGroup.Item>
-                            <ListGroup.Item><a href="pricing"><i class="fa fa-table"></i> <span>Pricing tables</span></a></ListGroup.Item>
-                            <ListGroup.Item><a href="blocks"><i class="fa fa-th"></i> <span>Content blocks</span></a></ListGroup.Item>
-                            <ListGroup.Item><a href="forms"><i class="fa fa-file-text"></i> <span>Forms</span></a></ListGroup.Item>
-                        </ListGroup>
+                        <ul class="nav nav-pills nav-stacked custom-nav">
+                            <li><a href="index"><i class="fa fa-tachometer"></i><span> Dashboard</span></a></li>
+                            <li><a href="cards"><i class="fa fa-cogs"></i> <span>Default cards</span></a></li>
+                            <li><a href="pricing"><i class="fa fa-table"></i> <span>Pricing tables</span></a></li>
+                            <li><a href="blocks"><i class="fa fa-th"></i> <span>Content blocks</span></a></li>
+                            <li><a href="forms"><i class="fa fa-file-text"></i> <span>Forms</span></a></li>
+                        </ul>
                         {/* <!-- //sidebar nav end --> */}
                         {/* <!-- toggle button start --> */}
-                        <a class="toggle-btn" onclick="toggleMenu()">
+                        <a class="toggle-btn" onclick={toggleMenu()}>
                             <i class="fa fa-angle-double-left menu-collapsed__left"><span>Collapse Sidebar</span></i>
                             <i class="fa fa-angle-double-right menu-collapsed__right"></i>
                         </a>
